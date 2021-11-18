f453302fb1396f19759d859ed0eb6add6281ef89
zarpy <dueduen@gmail.com>
2021-11-18 21:12:36 +0100

    Product as entity

--- src/main/java/Models/Store/Product.java
+++ src/main/java/Models/Store/Product.java
@@ -3,12 +3,71 @@ package Models.Store;
 /**
  * CREATED BY mathias @ 14-11-2021 - 14:07
  **/
-public class Product {
-    private  int productID;
+public class Product{
+
+    private int productID;
     private String productName;
     private String productDescription;
     private double productPrice;
     private int productsInStock;
     private byte[] ProductPicture;
 
+    public Product(){
+    }
+
+    public Product(int productID, String productName, String productDescription, double productPrice, int productsInStock){
+        this.productID = productID;
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.productPrice = productPrice;
+        this.productsInStock = productsInStock;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public int getProductsInStock() {
+        return productsInStock;
+    }
+
+    public void setProductsInStock(int productsInStock) {
+        this.productsInStock = productsInStock;
+    }
+
+    public byte[] getProductPicture() {
+        return ProductPicture;
+    }
+
+    public void setProductPicture(byte[] productPicture) {
+        ProductPicture = productPicture;
+    }
 }
